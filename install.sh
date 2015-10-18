ca860a1f08eeb41cf051117eafbdbeab90746e29
Joongi Kim <joongi@lablup.com>
2015-10-18 18:10:57 +0900

    Separate installation of aiobotocore as it does not provide pip repo.

--- install.sh
+++ install.sh
@@ -3,5 +3,7 @@ export PYENV_ROOT="/logger/.pyenv"
 export PATH="$PYENV_ROOT/bin:$PATH"
 eval "$(pyenv init -)"
 pyenv shell 3.5.0
+# aiobotocore does not have pip distribution yet.
+pip3 install git+https://github.com/jettify/aiobotocore.git@master
 python3 /logger/setup.py install --upgrade
 pyenv rehash
